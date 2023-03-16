228a539a8307465709b7ed2c6175962cfc64bd6b
EC2 Default User <ec2-user@ip-172-31-6-204.ap-northeast-1.compute.internal>
2023-03-16 15:42:31 +0000

    マイページindex/show/edit→退会ページと投稿ボタン未完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource)
+    public_customers_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,24 @@
 class Public::CustomersController < ApplicationController
+
   def index
+    @customers = Customer.all
+    @post = Post.new
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @posts = @customer.posts
+    @post = Post.new
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customers_path
   end
 
   def withdrawal_check
@@ -16,4 +26,11 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal
   end
+
+     private
+
+  def customer_params
+    params.require(:customer).permit(:name, :image, :address, :age, :oshi_name, :introduction)
+  end
+
 end
