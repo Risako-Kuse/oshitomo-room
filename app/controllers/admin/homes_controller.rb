62b54eb02a39e48344d83bdd2181dd7a8ec7eeb5
EC2 Default User <ec2-user@ip-172-31-6-204.ap-northeast-1.compute.internal>
2023-03-25 13:04:58 +0000

    ヘッダーとフッダーのレイアウト修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,14 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
+
   def top
+    @customers = Customer.all
   end
+
+   def destroy
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: !@customer.is_deleted) #有効か退会か反転させる
+    redirect_to admin_customers_path
+  end
+
 end
